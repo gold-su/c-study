bb56689580122fca3a2230094ded2e616929b0a4
gold-su <pds020130@naver.com>
2023-02-14 14:22:53 +0000

    grass

--- study/Chapter1.cpp
+++ study/Chapter1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>//화면에 출력하는 만들어둔 프로그램을 가져옴
 
 
 
@@ -6,6 +6,11 @@ int main(){
 	
 	std::cout << "hello, world" << std::endl;
 	
+	int x = 2;
+	x = 5;
+	int y = x + 3;
+	
+	std::cout << y << std::endl;
 	
 	return 0;
 	

7a8393475cd2f4faa8609861fb0d0bde627aa71b
gold-su <pds020130@naver.com>
2023-02-17 14:41:33 +0000

    grass

--- study/Chapter2.cpp
+++ study/Chapter2.cpp
@@ -1,18 +1,18 @@
 #include <iostream>//화면에 출력하는 만들어둔 프로그램을 가져옴
 
+//메모리에 숫자,문자열,문자 등등 저장하는데 이것을 객체라고함
+//객체는 메모리에 변수는 메모리에 담겨있는 객체를 사용하기 위한 이름, 또 주소의 다름 이기도 하다
+
 
 
 int main(){
 	
-	std::cout << "hello, world" << std::endl;
-	
-	int x = 2;
-	x = 5;
-	int y = x + 3;
-	
-	std::cout << y << std::endl;
+	int x;
+	x = 123; //assignment 할당하다
 	
 	
+	std::cout << x << std::endl;
+	std::cout << &x << std::endl;  //&변수를 실행하면 변수의 주소를 출력한다
 	
 	return 0;
 }

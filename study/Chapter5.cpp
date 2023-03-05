883c31513ae9009685d2cc639832b4f6b0079003
gold-su <pds020130@naver.com>
2023-03-05 13:43:59 +0000

    grass

---
+++ study/Chapter5.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+	
+	int x;  //밑의 cin에서 입력받은 데이터를 저장할수 있는 변수를 먼저 선언해야됨.
+	cin >> x;
+	
+	cout << "너가 입력한거 ㅋㅋ -> " << x << endl;  //저장할 변수가 int이기 때문에 정수만 저장이 가능하다
+	
+	return 0;
+}

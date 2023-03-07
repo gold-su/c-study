d52f44b3e561d36b3a34b6b163f6c39f4b2958c0
gold-su <pds020130@naver.com>
2023-03-07 13:34:06 +0000

    grass

--- study/Chapter5.cpp
+++ study/Chapter5.cpp
@@ -18,5 +18,8 @@ int main(){
 	cout << "after your a = " << a << endl;
 	cout << "입력한" << a <<" 로 바뀌었지?" << endl;
 	
+	// cin에 너무 큰 수를 입력하게 되면, int x나 a가 저장할 수 있는 가장 큰 수를 초과하기 때문에,
+	// 내가 의도한 수가 나오지 않을수 있다.
+	
 	return 0;
 }
---
+++ study/Chapter6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << 1 + 2 << endl;
+	cout << 5 + 7 << endl;
+	cout << 2 + 8 << endl;
+	
+	
+	return 0;
+}

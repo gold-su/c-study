f996c815db3fd09433b213d069199e1e30083fc6
gold-su <pds020130@naver.com>
2023-03-07 13:39:48 +0000

    grass

--- study/Chapter6.cpp
+++ study/Chapter6.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+int addTwoNumbers(int num_a, int num_b) //함수의 이름은 가급적 자세히 적는게 좋다.
+{
+	int sum = num_a + num_b ;
+	
+	return sum; //return하는 값과 함수의 타입이 값아야 함(int)
+}
+
 int main(){
-	cout << 1 + 2 << endl;
-	cout << 5 + 7 << endl;
+	
+	
+	cout << addTwoNumbers(1,2) << endl; //함수를 이용해서 같은 값을 출력할수 있음
+	cout << addTwoNumbers(2,5) << endl;
 	cout << 2 + 8 << endl;
 	
 	

47a2cd5457795a807e9899eeba657211de9e13a5
gold-su <pds020130@naver.com>
2023-03-09 14:45:09 +0000

    grass

--- study/Chapter8.cpp
+++ study/Chapter8.cpp
@@ -4,8 +4,16 @@ using namespace std;
 //식별자 = Identifier = 인식하다 
 //메모리 주소를 프로그래머가 인식할수 있도록 바꿔준다.
 
-int main(){
+int main(){  //main은 시스템이 찾는 첫번째 함수이다.
 	
+	int 3three(); //함수명은 숫자로 시작하면 안됨
+	int . , ; , //등도 안된다.
+	
+	int value; a; b; //많이 사용함, 가급적이면 너무 길지 않으면 의미를 포함하는 뱐수명이 좋음.
+	int num_apples; 나 n_apples 등
+		n_a
+		n_b //위와 같이 n_은 동일하게 가지고 가고 언더바 뒤로 차별을 두는 경우도 많다.
+		
 	
 	return 0;
 }

1e1f7ba9d45270e64b1ac467d5154c649cf1b7e2
gold-su <pds020130@naver.com>
2023-03-10 14:31:04 +0000

    grass

--- study/Chapter9.cpp
+++ study/Chapter9.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main(){
 	
-	int x(0); // x = 0;
-	s
+	int x(0); // x = 0; main의 중괄호를 넘어가면 x는 사라짐
+	cout << x << " " << &x << endl; //&는 메모리의 주소를 볼수 있다.
 	
-	return 0'
+	{
+		//중괄호 안에 중괄호도 가능
+		int x = 1 ;
+		cout << x << " " << &x << endl; 
+	}
+	// {
+	// 	int x = 2;   //중괄호마다 생성가능
+	// 	int x = 5;   //하지만 같은 곳 안에 똑같은 변수는 오류가 난다.
+	// }
+	cout << "출력시의 x의 값이랑 주소가 다르다.\n"
+	
+	return 0;
 }

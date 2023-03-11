a16020f58fcb0813f5e71cc76a9bf02540b3dd0e
gold-su <pds020130@naver.com>
2023-03-11 13:32:40 +0000

    grass

---
+++ study/Chapter2-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+	//리터럴 Literal
+	//피연산자 Operand
+	//단항 unary , 이항 binary, 삼항 ternary
+
+int main(){
+	
+	int x = 2; // x is a variable(변수,변하기쉬운), 2 is a literal(그냥 숫자,정확한).
+	
+	cout << 1 + 2 << endl; //1,2은 Literal, +는 연산자
+	
+	cout << "전역마렵다\n"; //""(문자열)도 Literal
+	
+	return 0;
+}

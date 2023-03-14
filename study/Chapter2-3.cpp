627e91c881fcf2b751f452f31a6a75bb5137ecb8
gold-su <pds020130@naver.com>
2023-03-14 14:14:56 +0000

    grass

--- study/Chapter2-3.cpp
+++ study/Chapter2-3.cpp
@@ -3,15 +3,20 @@
 using namespace std;
 
 
-int add(int x, int y) {
+int add(int x, int y) 
+{			  //탭으로 빈칸 만드는걸 indenting이라고 함	
 	return x + y ;
 }
-
+                                                             
 int main(){
 	
 	cout << "ddd" <<  
 		"ddddd" << endl; 
 	
+	
+	int my_V 		=1;          //이런식으로 줄을 맞춰서 선언하는게 보기 더 좋다.
+	int x 			=4;
+	int num_apples 	=123;
 	
 	return 0;
 }

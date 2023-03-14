bb76dafcc0eb2d0726528d058ef1b39cb4f953d8
gold-su <pds020130@naver.com>
2023-03-14 14:16:29 +0000

    grass

--- study/Chapter2-3.cpp
+++ study/Chapter2-3.cpp
@@ -18,5 +18,12 @@ int main(){
 	int x 			=4;
 	int num_apples 	=123;
 	
+	int x = 1 + 2; //this is important
+	//한줄씩 띄어주는게 좋음 
+	int x = 1 + 2; //this is important
+	
+	
+	//그냥 같이 일하는 사람이 내 코드를 봤을때 깔끔하고 보기좋은게 최고다
+	
 	return 0;
 }

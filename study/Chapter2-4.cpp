988c58a6b362ab38ef0c3147fca17b33bce3afcc
gold-su <pds020130@naver.com>
2023-05-22 14:19:19 +0000

    grass

--- study/Chapter2-4.cpp
+++ study/Chapter2-4.cpp
@@ -1,9 +1,20 @@
-#intclude <iostream>
+#include <iostream>
 
 using namespace std;
 
+int Tham(int a, int b)
+{
+	int sum = a + b;
+	
+	return sum;
+	
+}
+
+
 int main(){
 	
+	
+	cout << Tham(1,2) << endl;
 	
 	return 0;
 }

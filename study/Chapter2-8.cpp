d6a859512966457d01063bd2a79a06969845c64d
gold-su <pds020130@naver.com>
2023-06-01 02:25:05 +0000

    grass

--- study/Chapter2-8.cpp
+++ study/Chapter2-8.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
 
-
-
-int add(int a, int b)
+namespace Myspace1 //namespce로 선언 후 함수 이름이 중복되는 함수를 안에 넣어서 같이 활용할수 있음
 {
+	int add(int a, int b)
+	{
 	return a + b;
 	
+	}
+	
 }
+
+
 
 int add(int a, int b)
 {
 	return a * b;
 	
 }
-int mian(){
+using namespace std;
+
+int main(){
 	
-	cout << add(1,2) << endl;
-	
+	cout << Myspace1::add(3,5) << endl;  //namespace 안에 있는 중복되는 함수는 namespace 이름 :: 함수 이름 << 이렇게 사용할수 있음
+	cout << add(3,5) << endl;
 	
 	return 0;
 }
